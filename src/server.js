fa7c27bbafb80253120655117044932e74f31d6f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-05-02 16:49:56 -0300

    chore: added http metadata header to response of route & saved users in memory

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
   const { method, url } = request
 
   if (method === 'GET' && url === '/users') {
-    return response.end('List of users')
+    return response
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
     return response.end('Creation of user')
   }
 
-  return response.end('Hello World!')
+  return response.end('🚀 Server is running at http://localhost:3333')
 })
 
 server.listen(3333)
