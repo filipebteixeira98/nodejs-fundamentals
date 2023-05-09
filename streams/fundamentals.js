a65d6a1ce2bfda6c1299fc365aef938484e83953
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-05-09 11:28:53 -0300

    chore: created transform stream to modify data

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,5 +1,5 @@
 // process.stdin.pipe(process.stdout)
-import { Readable, Writable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
   index = 1
@@ -19,6 +19,14 @@ class OneToHundredStream extends Readable {
   }
 }
 
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformedChunk = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformedChunk)))
+  }
+}
+
 class MultiplyByTenStream extends Writable {
   _write(chunk, encoding, callback) {
     console.log(Number(chunk.toString()) * 10)
@@ -27,4 +35,6 @@ class MultiplyByTenStream extends Writable {
   }
 }
 
-new OneToHundredStream().pipe(new MultiplyByTenStream())
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
